646518e24c998284f2ebeb6a4612d880a7c55f0b
Jack <xuwenjiejack@gmail.com>
2018-06-14 11:09:16 +0800

    Add RSApplication into project.

---
+++ Sources/RSApplication.h
@@ -0,0 +1,13 @@
+//
+//  RSApplication.h
+//  RealSimulator
+//
+//  Created by Jack on 2018/6/14.
+//  Copyright © 2018 XWJACK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSApplication : UIApplication
+
+@end
---
+++ Sources/RSApplication.m
@@ -0,0 +1,13 @@
+//
+//  RSApplication.m
+//  RealSimulator
+//
+//  Created by Jack on 2018/6/14.
+//  Copyright © 2018 XWJACK. All rights reserved.
+//
+
+#import "RSApplication.h"
+
+@implementation RSApplication
+
+@end
